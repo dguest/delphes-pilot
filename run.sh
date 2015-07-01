81b13bade39fb3bca281b817a79d9f52a328c449
Chase Shimmin <cshimmin@uci.edu>
2015-06-30 18:32:45 -0700

    add some command line options

--- run.sh
+++ run.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
-input_file=$1
-output_file=$2
+show_help() {
+	echo "Setup staging area and run pythia/delphes in isolation."
+	echo "Usage: $0 [options] input_file"
+	echo "  -o OUTPUT_PATH    The destination path for output file."
+	echo "  -n OUTPUT_NAME    The output filename."
+}
 
-if [ -z "$input_file" ]; then
-	echo "No input file provided! Abort."
+output_name=delphes.root
+
+while getopts ":h?o:n:" opt; do
+    case "$opt" in
+    h) show_help
+       exit 0
+       ;;
+    o) output_path=$OPTARG
+       ;;
+    n) output_name=$OPTARG
+       ;;
+   \?) echo "Invalid option: -$OPTARG" >&2
+       show_help
+       exit 1
+       ;;
+    :) echo "Option: -$OPTARG requires argument." >&2
+       show_help
+       exit 1
+       ;;
+    esac
+done
+
+shift $((OPTIND-1))
+input_file=$1
+
+if [ $# -lt 1 ]; then
+	echo "No input file provided! Abort." >&2
+	show_help
 	exit 1
 fi
 
 if [ ! -e "$input_file" ]; then
-	echo "Cannot find input file. Abort."
+	echo "Cannot find input file. Abort." >&2
 	exit 1
 fi
 
-if [ -z "$output_file" ]; then
-	output_file=$(dirname $input_file)/delphes.root
-	echo "No output file provided, using $output_file"
+if [ -z "$output_path" ]; then
+	output_path=$(dirname $input_file)
+	echo "No output path provided, using $output_path"
 fi
 
 if [ ! -z "$SLURM_JOB_ID" ]; then
@@ -28,9 +58,11 @@ else
 fi
 
 if [ ! -d "$stage_path" ]; then
-	echo "Error creating staging path in $stage_path! Abort."
+	echo "Error creating staging path in $stage_path! Abort." >&2
 	exit 1
 fi
+
+output_file=$output_path/$output_name
 
 # setup the working directory for pythia and delphes to run in
 echo "Staging in $stage_path"
--- submit.sh
+++ submit.sh
@@ -1,5 +1,71 @@
 #!/bin/bash
 
+show_help() {
+	echo "Submit pythia/delphes jobs to the slurm cluster."
+	echo "Usage: $0 [options] input_file [input_file ...]"
+	echo "Options:"
+	echo "  -t TAG_NAME     A prefix tag for output files."
+	echo "  -c N_CORES      Number of cores to request per job."
+	echo "  -p PARTITION    The slurm partition to submit to."
+	echo "  -q              Squelch slurm logs."
+}
+
+# defaults
+ncores=2
+quiet=0
+partition=atlas_all
+
+# parse CLI
+while getopts ":h?t:c:p:q" opt; do
+    case "$opt" in
+    h)  show_help
+        exit 0
+        ;;
+    t)  tag=$OPTARG
+        ;;
+    c)  ncores=$OPTARG
+        ;;
+    p)  partition=$OPTARG
+        ;;
+    q)  quiet=1
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+	show_help
+	exit 1
+	;;
+    :)
+        echo "Option: -$OPTARG requires argument." >&2
+	show_help
+	exit 1
+	;;
+    esac
+done
+
+# shift to arguments to list of input files
+shift $((OPTIND-1))
+
+if [ $# -lt 1 ]; then
+	echo "No input files provided! Abort." >&2
+	show_help
+	exit 1
+fi
+
+# add a tag to the input filename, if requested.
+output_name=delphes.root
+if [ ! -z "$tag" ]; then
+	output_name=${tag}_${output_name}
+fi
+
+# setup options to sbatch command
+SLURM_OPTS="-c $ncores -p $partition -t 120"
+
+if [ "$quiet" == "1" ]; then
+	SLURM_OPTS+=" -o /dev/null"
+fi
+
+# schedule a job for each input file
 for input_file in $@; do
-	sbatch -c 2 -p atlas_all -t 120 ./run.sh $input_file
+	echo "Submitting job for $input_file"
+	sbatch $SLURM_OPTS ./run.sh -n $output_name $input_file
 done
