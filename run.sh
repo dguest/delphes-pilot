f3ac7ba289d2495e748d6a8c01d35428bb803f33
Chase Shimmin <cshimmin@uci.edu>
2015-10-18 09:04:21 -0700

    Add option to specify delphes card and output tags
    Also add options for verbosity, recursive file-finding, and time limits.

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-output_name=delphes.root
-
 show_help() {
 	echo "Setup staging area and run pythia/delphes in isolation."
 	echo "Usage: $0 [options] input_file"
 	echo "  -o OUTPUT_PATH    The destination path for output file. (Default: same path as input file)"
-	echo "  -n OUTPUT_NAME    The output filename. (Default: $output_name)"
+	echo "  -n OUTPUT_NAME    The output filename. (Default: based on input_file name)"
+	echo "  -t OUTPUT_TAG     A prefix tag for the output filename."
+	echo "  -d DELPHES_CARD   Path to the desired delphes card to run."
 }
 
-while getopts ":h?o:n:" opt; do
+while getopts ":h?o:n:t:d:" opt; do
     case "$opt" in
     h) show_help
        exit 0
@@ -17,6 +17,10 @@ while getopts ":h?o:n:" opt; do
     o) output_path=$OPTARG
        ;;
     n) output_name=$OPTARG
+       ;;
+    t) output_tag=$OPTARG
+       ;;
+    d) delphes_card=$OPTARG
        ;;
    \?) echo "Invalid option: -$OPTARG" >&2
        show_help
@@ -35,6 +39,16 @@ input_file=$1
 if [ $# -lt 1 ]; then
 	echo "No input file provided! Abort." >&2
 	show_help
+	exit 1
+fi
+
+# source the configuration environment
+if [ -e config.sh ]; then
+	source config.sh
+fi
+
+if [ -z "$DELPHES_DIR" ]; then
+	echo "DELPHES_DIR variable not defined!" >&2
 	exit 1
 fi
 
@@ -62,36 +76,38 @@ if [ ! -d "$stage_path" ]; then
 	exit 1
 fi
 
-output_file=$output_path/$output_name
+clean_stage() {
+	echo "Removing staging area at $stage_path"
+	rm -rf $stage_path
+}
 
-# source the configuration environment
-if [ -e config.sh ]; then
-	source config.sh
+
+if [ -z "$output_name" ]; then
+	output_name=$(basename $input_file)
+	output_name=${output_name%.lhe*}
+	output_name=${output_name#*events}
+	output_name="delphes${output_name}.root"
+	if [ ! -z "$output_tag" ]; then
+		output_name="${output_tag}_${output_name}"
+	fi
 fi
+
+output_file=$output_path/$output_name
 
 if [ -z "$PYTHIA_DIR" ]; then
 	echo "PYTHIA_DIR variable not defined!" >&2
 	exit 1
 fi
 
-if [ -z "$DELPHES_DIR" ]; then
-	echo "DELPHES_DIR variable not defined!" >&2
-	exit 1
-fi
-
-clean_stage() {
-	echo "Removing staging area at $stage_path"
-	rm -rf $stage_path
-}
-
 # setup the working directory for pythia and delphes to run in
 echo "Staging in $stage_path"
-./scripts/stage.sh $input_file $stage_path
+./scripts/stage.sh $input_file $stage_path $delphes_card
+stage_code=$?
 
-if [ $? -ne 0 ]; then
-	echo "Staging failed!" >&2
+if [ $stage_code -ne 0 ]; then
+	echo "Staging failed! Code=$stage_code" >&2
 	clean_stage
-	exit 1
+	exit $stage_code
 fi
 
 # move to the stage and run the processing scripts
@@ -118,5 +134,8 @@ if [ $? -ne 0 ]; then
 	clean_stage
 	exit 1
 fi
+
+echo "cp $stage_path/Cards/delphes_card.dat $output_file.card"
+cp $stage_path/Cards/delphes_card.dat $output_file.card
 
 clean_stage
--- submit.sh
+++ submit.sh
@@ -2,26 +2,36 @@
 
 # defaults
 ncores=2
-quiet=0
+quiet=1
 recurse=0
 partition=atlas_all
+tlimit=30
 
 show_help() {
 	echo "Submit pythia/delphes jobs to the slurm cluster."
 	echo "Usage: $0 [options] input_file [input_file ...]"
 	echo "Options:"
+	echo "  -r              Recursively find unweighted_events.lhe(.gz) files in the given directory."
+	echo "  -m MAX_JOB      Maximum number of files to process. (For use with -r only)"
+	echo "  -d DELPHES_CARD Path to the desired delphes card to run."
 	echo "  -t TAG_NAME     A prefix tag for output files (e.g. <TAG_NAME>_delphes.root)."
 	echo "  -c N_CORES      Number of cores to request per job. (Default: $ncores)"
 	echo "  -p PARTITION    The slurm partition to submit to. (Default: $partition)"
-	echo "  -r              Recursively find unweighted_events.lhe(.gz) files in the given directory."
-	echo "  -q              Squelch slurm logs."
+	echo "  -l TLIMIT       The time limit (in minutes). (Default: $tlimit)"
+	echo "  -v              Don't squelch slurm logs."
 }
 
 # parse CLI
-while getopts ":h?t:c:p:rq" opt; do
+while getopts ":h?rm:d:t:c:p:l:v" opt; do
     case "$opt" in
     h)  show_help
         exit 0
+        ;;
+    r)  recurse=1
+        ;;
+    m)  max_job=$OPTARG
+        ;;
+    d)  delphes_card=$OPTARG
         ;;
     t)  tag=$OPTARG
         ;;
@@ -29,9 +39,9 @@ while getopts ":h?t:c:p:rq" opt; do
         ;;
     p)  partition=$OPTARG
         ;;
-    r)  recurse=1
+    l)  tlimit=$OPTARG
         ;;
-    q)  quiet=1
+    v)  quiet=0
         ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -58,24 +68,35 @@ fi
 if [ "$recurse" == "0" ]; then
 	input_files=$@
 else
-	input_files=$(find $1 -regex ".*unweighted_events\.lhe\(\.gz\)?")
-fi
-
-# add a tag to the input filename, if requested.
-output_name=delphes.root
-if [ ! -z "$tag" ]; then
-	output_name=${tag}_${output_name}
+	search_str=".*\(unweighted_events\|sherpa_events\)\(\._.*\)?\.\(lhe\|hepmc2g\)\(\.gz\)?"
+	if [ -z "$max_job" ]; then
+		input_files=$(find -L $1 -regex $search_str)
+	else
+		input_files=$(find -L $1 -regex $search_str | sort | head -n $max_job)
+	fi
 fi
 
 # setup options to sbatch command
-SLURM_OPTS="-c $ncores -p $partition -t 120"
+SLURM_OPTS="-c $ncores -p $partition -t $tlimit"
 
 if [ "$quiet" == "1" ]; then
 	SLURM_OPTS+=" -o /dev/null"
 fi
 
+if [ "$recurse" == "1" ]; then
+	SLURM_OPTS+=" -J $(basename $1)"
+fi
+
+RUN_OPTS=""
+if [ ! -z "$delphes_card" ]; then
+	RUN_OPTS+=" -d $delphes_card"
+fi
+if [ ! -z "$tag" ]; then
+	RUN_OPTS+=" -t $tag"
+fi
+
 # schedule a job for each input file
 for input_file in $input_files; do
 	echo "Submitting job for $input_file"
-	sbatch $SLURM_OPTS ./run.sh -n $output_name $input_file
+	sbatch $SLURM_OPTS ./run.sh $RUN_OPTS $input_file
 done
